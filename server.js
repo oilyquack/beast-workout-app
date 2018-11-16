ee1db785e2cfdc95740a4a400fac87571b2fcb24
Ollie Crook <olliejcrook@gmail.com>
2018-11-16 10:22:14 +0000

    connect db

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require("express");
+require("dotenv").config();
+
 const bodyParser = require("body-parser");
+const pgp = require("pg-promise")();
+const express = require("express");
 const app = express();
+const db = pgp({
+  host: "localhost",
+  port: 5432,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+});
 
 app.use(bodyParser.json());
+
 app.use("/static", express.static("static"));
 app.set("view engine", "hbs");
 
