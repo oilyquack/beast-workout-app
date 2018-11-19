9cc5bfb098b39eb0f2d65ab53aa0f4a4ace602e3
Ollie Crook <olliejcrook@gmail.com>
2018-11-19 11:28:44 +0000

    implement react-router with sessions page and landing page on "/"

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import SessionsContainer from "../containers/SessionsContainer";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
-      <main>
-        <SessionsContainer />
-      </main>
+      <BrowserRouter>
+        <main>
+          <Link to={`/sessions`}>Session</Link>
+          <Route exact path="/" render={() => <h1>BEAST!</h1>} />
+          <Route path="/sessions" component={SessionsContainer} />
+        </main>
+      </BrowserRouter>
     );
   }
 }
