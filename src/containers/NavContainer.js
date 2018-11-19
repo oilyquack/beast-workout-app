f43cb187a13727d8bc10bca4641a1a8b3608626e
Ollie Crook <olliejcrook@gmail.com>
2018-11-19 13:04:29 +0000

    add nav container

---
+++ src/containers/NavContainer.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+import Nav from "../components/Nav";
+
+export const mapStateToProps = state => {
+  return {};
+};
+
+export const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Nav);
